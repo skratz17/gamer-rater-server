c8bc108aa5a235460f3831a6d95da6d9247cfc0b
Jacob Eckert <jweckert17@gmail.com>
2020-11-04 14:34:15 -0600

    specify media location in django settings, configure static urls, and specify target directory for game_image

--- raterapp/models/game_image.py
+++ raterapp/models/game_image.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class GameImage(models.Model):
     """GameImage database model"""
-    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    image = models.ImageField(upload_to='game_images', height_field=None, width_field=None, max_length=None, null=True)
     game = models.ForeignKey('raterapp.Game', on_delete=models.CASCADE)
     player = models.ForeignKey('raterapp.Player', on_delete=models.CASCADE)
--- server/urls.py
+++ server/urls.py
@@ -1,6 +1,8 @@
 """Route Configuration"""
 from django.urls import path
+from django.conf import settings
 from django.conf.urls import include
+from django.conf.urls.static import static
 from rest_framework import routers
 from raterapp.views import login_user, register_user, Games, Categories, Designers, GameReviews
 
@@ -15,4 +17,4 @@ urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
